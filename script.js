c2fdea685b08763051ada414cc5e53032e15a4ea
amnaakhtar1213 <amnabinterasheed1911@gmail.com>
2026-04-12 12:31:18 +0500

    update script file

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function converter() {
      if(tofehrenheit.checked){
         temp = Number(textBox.value)
         temp = temp * 9 / 5 + 32;
-        result.textContent = temp.toFixed(1) + "F"
+        result.textContent = temp.toFixed(1) + " °F"
 } else if(toCelsius.checked){
         temp = Number(textBox.value);
         temp = (temp - 32) * (5/9)
-        result.textContent = temp.toFixed(1) + "C"
+        result.textContent = temp.toFixed(1) + " °C"
 } else {
         result.textContent = "Select a unit first"
 }
